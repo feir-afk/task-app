5b06fcc92dabb8d63661018b104f0198450dd546
Riefqi Kamal <riefqikamal369@gmail.com>
2024-02-23 13:01:29 +0800

    fix: cors

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,20 +1,40 @@
 import { connect_to_database } from "@/db";
 import { Task } from "@/models/schema";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // Update a task
-export async function PUT(request: any, { params }: any) {
+export async function PUT(request: NextRequest, { params }: any) {
 	const { id } = params;
 	const { newTask: task } = await request.json();
 	await connect_to_database();
 	await Task.findByIdAndUpdate(id, { task });
-	return NextResponse.json({ message: "Task Updated" }, { status: 200 });
+	return NextResponse.json(
+		{ message: "Task Updated" },
+		{
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		}
+	);
 }
 
 // Find a task
-export async function GET(request: any, { params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
 	const { id } = params;
 	await connect_to_database();
 	const task = await Task.findOne({ _id: id });
-	return NextResponse.json({ task }, { status: 200 });
+	return NextResponse.json(
+		{ task },
+		{
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		}
+	);
 }
--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,26 +1,46 @@
 import { connect_to_database } from "@/db";
 import { Task } from "@/models/schema";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // Create a task
-export async function POST(request: any) {
+export async function POST(request: NextRequest) {
 	const { task } = await request.json();
 	await connect_to_database();
 	await Task.create({ task });
-	return NextResponse.json({ message: "Task Created" }, { status: 201 });
+	return NextResponse.json(
+		{ message: "Task Created" },
+		{
+			status: 201,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		}
+	);
 }
 
 // Read all tasks
-export async function GET(request: any) {
+export async function GET(request: NextRequest) {
 	await connect_to_database();
 	const tasks = await Task.find();
 	return NextResponse.json({ tasks });
 }
 
 // Delete a task by specific Id
-export async function DELETE(request: any) {
+export async function DELETE(request: NextRequest) {
 	const id = request.nextUrl.searchParams.get("id");
 	await connect_to_database();
 	await Task.findByIdAndDelete(id);
-	return NextResponse.json({ message: "Task Deleted" }, { status: 200 });
+	return NextResponse.json(
+		{ message: "Task Deleted" },
+		{
+			status: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			},
+		}
+	);
 }
